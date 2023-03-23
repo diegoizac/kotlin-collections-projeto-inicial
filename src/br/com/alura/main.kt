c96365bf388d437846f6fa4f43778e4d989c6b76
Diêgo Izac Luis da Silva <diegoizac@hotmail.com>
2023-03-23 00:30:40 -0300

    Add do map de pedidos

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -1,5 +1,14 @@
 package br.com.alura
 
 fun main() {
-    testaSet()
+    val pedidos = mapOf(Pair(1, 20.0), Pair(2, 34.0), Pair(3, 50.0))
+    println(pedidos)
+    val pedido = pedidos[3]
+    pedido?.let {
+        println("pedido $it")
+    }
+    for (p: Map.Entry<Int, Double> in pedidos) {
+        println("número do pedido: ${p.key}") //chave
+        println("valor do pedido: ${p.value}") //valor
+    }
 }
