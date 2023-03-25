d1e42f99216b252bf8bbd438d2aae0c0188d23f0
Diêgo Izac Luis da Silva <diegoizac@hotmail.com>
2023-03-25 01:32:49 -0300

    add Utilizando o agrupamento
     Utilize o groupBy() para criar um mapa de lista de pedidos, a chave deve ser do tipo Boolean que determina true como frete grátis e false como sem.
    
     Utilize a mesma regra do exercício anterior para indicar que tem o frete grátis ou não.
    
     Então, imprima o mapa e busque os pedidos que tenham frete grátis e os que não tenham a partir da chave do tipo Boolean.
    
     Em seguida, crie uma lista de nomes e utilize o groupBy() para criar uma agenda de nomes e agrupá-los pela letra inicial.
    
     Então, imprima a agenda e busque pessoas da agenda a partir da letra inicial, ou seja, a partir de um Char
    
     Por fim, teste o programa e confira se ele apresenta o resultado esperado em ambos os mapas.

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -5,7 +5,8 @@ fun main() {
         Pedido(1, 20.0),
         Pedido(2, 60.0),
         Pedido(3, 30.0),
-        Pedido(4, 70.0)
+        Pedido(4, 70.0),
+        Pedido(5, 40.0)
     )
     println(pedidos)
     val pedidosMapeados: Map<Int, Pedido> = pedidos.associateBy { pedido: Pedido -> // associateBy está pegando os pedidos associados pelo numero do pedido e retorna um map com numero do pedido e o valor do pedido
@@ -18,6 +19,33 @@ fun main() {
         pedido.valor > 50.0
     }
     println(pedidosFreteGratis)
-    println(pedidosFreteGratis[Pedido(numero = 1, valor = 60.0)]) //retorna true pq o valor do pedido é maior que 50
+    println(pedidosFreteGratis[Pedido(numero = 2, valor = 60.0)]) // retorna true pq o valor do pedido é maior que 50
+
+//      val mapa: Map<Boolean, Pedido> = pedidos.associateBy { pedido ->
+//          pedido.valor > 50.0
+//      } // retorna um map com chave boolean e valor pedido
+//          println(mapa)
+    val pedidosFretreGratisAgrupados: Map<Boolean, List<Pedido>> =
+            pedidos.groupBy { pedido: Pedido -> // groupBy está pegando os pedidos e agrupando por regra de negocio e retorna um map com chave boolean e valor lista de pedidos
+                pedido.valor > 50.0
+    }
+    println(pedidosFretreGratisAgrupados) // retorna um map com chave boolean e valor lista de pedidos
+    println(pedidosFretreGratisAgrupados[true]) // retorna uma lista de pedidos com valor maior que 50
+
+    val nomes: List<String> = listOf(
+        "Eduardo",
+        "Alex",
+        "Fran",
+        "Gui",
+        "Ana"
+    )
+
+    val agenda: Map<Char, List<String>> = nomes.groupBy { nome: String -> // groupBy está pegando os nomes e agrupando por primeira letra do nome e retorna um map com chave char e valor lista de nomes
+        nome.first() // agrupa por primeira letra do nome
+    }
+
+    println(agenda)
+    println(agenda['A']) // retorna uma lista de nomes que começam com a letra A
+
 }
 data class Pedido(val numero: Int, val valor: Double)
