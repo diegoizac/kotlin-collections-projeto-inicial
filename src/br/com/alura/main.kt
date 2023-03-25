0845f97569379e443f4802d8c3b68e10a63f0fb9
Diêgo Izac Luis da Silva <diegoizac@hotmail.com>
2023-03-25 00:58:55 -0300

    add Utilizando a associação  Criar um mapa de pedidos com a chave sendo o número do pedido.  Criar um mapa de Boolean com a chave sendo os pedidos para verificar se têm frete grátis (true) ou não (false).  Lembre-se de representar o pedido com uma classe. Considere o uso da data class para obter um toString() pronto.
     Neste exercício, considere o uso das seguintes funções associate() ou associateBy() e o associateWith()

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -1,5 +1,23 @@
 package br.com.alura
 
 fun main() {
-    testaComportamentosMap()
+    val pedidos: List<Pedido> = listOf(
+        Pedido(1, 20.0),
+        Pedido(2, 60.0),
+        Pedido(3, 30.0),
+        Pedido(4, 70.0)
+    )
+    println(pedidos)
+    val pedidosMapeados: Map<Int, Pedido> = pedidos.associateBy { pedido: Pedido -> // associateBy está pegando os pedidos associados pelo numero do pedido e retorna um map com numero do pedido e o valor do pedido
+        pedido.numero
+    }
+    println(pedidosMapeados)
+    println(pedidosMapeados[1])
+
+    val pedidosFreteGratis: Map<Pedido, Boolean> = pedidos.associateWith { pedido: Pedido -> // associateWith está pegando os pedidos associados pela regra de negocio e retorna um map com o pedido e o valor da regra de negocio
+        pedido.valor > 50.0
+    }
+    println(pedidosFreteGratis)
+    println(pedidosFreteGratis[Pedido(numero = 1, valor = 60.0)]) //retorna true pq o valor do pedido é maior que 50
 }
+data class Pedido(val numero: Int, val valor: Double)
